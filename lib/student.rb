0d562bac077acb8757d6eec5a645908d6fa717cd
John Werenski <jtwerenski@gmail.com>
2018-09-29 21:02:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,12 @@
 require_relative "./user.rb"
 class Student < User
+  attr_accessor :knowledge
+  
   def initialize
     @knowledge = []
+  end 
+  
+  def learn(string)
+    self.knowledge << string 
+  end 
 end
